40e6618a8e2b64618df3a8410551ffc5b13492bd
Lukas Mohs <ga47boq@mytum.de>
2017-04-18 14:15:18 -0400

    first commit

---
+++ image-classifier.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+imgcodes = range(1,7)
+for imgcode in imgcodes:
+    fileName = str(imgcode) + '.jpeg'
+    img = cv2.imread(fileName,0)
+    img2 = img.copy()
+    template = cv2.imread('a.jpeg',0)
+    w, h = template.shape[::-1]
+
+    # All the 6 methods for comparison in a list
+ #  methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+#                'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+
+    methods = ['cv2.TM_SQDIFF_NORMED']
+    for meth in methods:
+        img = img2.copy()
+        method = eval(meth)
+
+        # Apply template Matching
+        res = cv2.matchTemplate(img,template,method)
+
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        print("min: " + str(min_loc))
+        print("max: " + str(max_loc))
+
+        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+
+        cv2.rectangle(img,top_left, bottom_right, 255, 2)
+        plt.subplot(121),plt.imshow(res,cmap = 'gray')
+        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122),plt.imshow(img,cmap = 'gray')
+        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+        plt.suptitle(meth)
+
+        plt.show()
