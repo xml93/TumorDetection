2b1e36b60b5b25ac673a913238b442209619c958
Lukas Mohs <ga47boq@mytum.de>
2017-04-19 00:15:08 -0400

    improved cropping

--- cropImages.py
+++ cropImages.py
@@ -2,28 +2,36 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+def plot():
+    start = (startX, startY)
+    end = (endX, endY)
+    cv2.rectangle(img,start, end, 255, 2)
+    plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    plt.show()
+
 img=cv2.imread('1.jpeg')
 height, width, channels = img.shape
 startX = width//2
-startY = 0
+startY = height//2
 endX = 0
 endY = height//2
-for x in range(width):
-    for y in range(height):
-        if img[x][y][0] > 100 or img[x][y][1] > 100 or img[x][y][2] > 100:
-            if startY == 0:
-                startY=y
+for y in range(height):
+    for x in range(width):
+        if img[y][x][0] > 30 or img[y][x][1] > 30 or img[y][x][2] > 30:
+            if startY > y:
+                startY = y
+                print(str(img[x][y][0]))
+                print(str(img[x][y][1]))
+                print(str(img[x][y][2]))
+                print("___")
             if startX > x:
-                startY = x
+                startX = x
             if endX < x:
                 endX = x
-            if endY > y:
+            if endY < y:
                 endY = y
             #print(str(x) + ":" + str(y))
             #print(str(img[x][y]))
-
-start = (startX, startY)
-end = (endX, endY)
-cv2.rectangle(img,start, end, 255, 2)
-plt.subplot(122),plt.imshow(img,cmap = 'gray')
-plt.show()
+print("X:" + str(startX))
+print("Y:" + str(startY))
+plot()
